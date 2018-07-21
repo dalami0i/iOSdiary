2a2b8d7b873884fc8c27a4adfc38287a0b849c5e
Edward WonSangjin <edward@edwardui-macbook-pro.local>
2018-07-21 11:30:58 +0900

    Initial Commit

---
+++ MyTest180721/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTest180721
+//
+//  Created by Edward WonSangjin on 2018. 7. 21..
+//  Copyright © 2018년 Edward WonSangjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
