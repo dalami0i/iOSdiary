8e1b4674dcae9aa5f29e218897ed64c1f2cb8667
Edward WonSangjin <edward@Edwardui-MacBook-Pro.local>
2018-07-21 15:15:27 +0900

    git first test

--- MyTest180721/Sources/Controllers/ViewController.swift
+++ MyTest180721/Sources/Controllers/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    //메모리 관리하는 부분으로 일반적으로 잘 안쓰인다고 함
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
+
+let vc = ViewController();
 
